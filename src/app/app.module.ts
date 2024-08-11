e311cebee78cb1a3de9e102a0d93b3a402677464
kytynutza <cristinamamukadze@gmail.com>
2024-08-11 15:23:43 +0200

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
-
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesModule } from './heroes/heroes.module';
-//import { HeroComponent } from './heroes/hero/hero.component';
-//import { ListComponent } from './heroes/list/list.component';
+
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    //HeroComponent,
-   // ListComponent
+
+    AppComponent
 
   ],
 
@@ -25,7 +24,8 @@ import { HeroesModule } from './heroes/heroes.module';
     BrowserModule,
    AppRoutingModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
 
 
   ],
